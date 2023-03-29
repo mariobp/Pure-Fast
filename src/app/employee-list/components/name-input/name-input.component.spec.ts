f49fa4ebedd0888c21b86ba8684afee748f52f2d
Mario Barrios <mariobarrios@rccl.com>
2023-03-28 22:25:26 -0500

    update to angular 11

--- src/app/employee-list/components/name-input/name-input.component.spec.ts
+++ src/app/employee-list/components/name-input/name-input.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { NameInputComponent } from './name-input.component';
 
@@ -6,7 +6,7 @@ describe('NameInputComponent', () => {
   let component: NameInputComponent;
   let fixture: ComponentFixture<NameInputComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NameInputComponent ]
     })
